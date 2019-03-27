b51147d96565210f1c256d772a01015a982b837a
Oksana Samkova <oksasamkova@gmail.com>
2019-03-27 16:39:15 -0400

    added score

--- src/Flipcards.jsx
+++ src/Flipcards.jsx
@@ -5,12 +5,37 @@ import { useSpring, animated as a } from 'react-spring';
 const LIGHT_TEAL = '#b6dbd0';
 const YELLOW = '#FFFF65';
 
-const StyledCardsContainer = styled.div`
-    opacity : ${props => props.completed ? '.3' : '1'}
+const StyledContainer = styled.div`
     display : grid;
-    grid-template-columns: repeat(4, minmax(20px, 120px));
     grid-gap: 10px;
     margin : 20px;
+    font-family : 'Work Sans', sans-serif;
+	font-weight : 300;
+    font-size : 18px;
+    color : rgb(77, 77, 77);
+`;
+
+const StyledHeaderContainer = styled(StyledContainer)`
+    grid-template-columns: repeat(4, minmax(20px, 120px));
+
+    > div {
+        display: flex;
+        justify-content: center;
+        padding: 0 8%;
+
+        > * {
+            flex : 1 auto;
+        }
+    }
+
+    div:last-child {
+        grid-column : 4 / 5;
+    }
+`;
+
+const StyledCardsContainer = styled(StyledContainer)`
+    opacity : ${props => props.running ? '1' : '.3'}
+    grid-template-columns: repeat(4, minmax(20px, 120px));
     height: calc( 100vh - 2 * 40px );
 `;
 
@@ -25,19 +50,14 @@ const StyledCardContainer = styled.div`
 const StyledAlertContainer = styled(StyledCardsContainer)`
     opacity : 1;
     position: absolute;
-    top : 0;
+    top : 50px;
     grid-template-rows: repeat(4, minmax(40px, auto));
-    color : rgb(77, 77, 77);
-	font-family : 'Work Sans', sans-serif;
-	font-weight : 300;
-    font-size : 18px;
 `;
 
 const StyledCard = styled(Card)(props => {
     return ({
         backgroundColor: props.color,
         borderRadius : '4px',
-        padding : '4px',
         backgroundImage: props.isFront ? 'unset' :
             `repeating-linear-gradient(120deg, rgba(255,255,255,.1), rgba(255,255,255,.1) 1px, transparent 1px, transparent 60px),
             repeating-linear-gradient(60deg, rgba(255,255,255,.1), rgba(255,255,255,.1) 1px, transparent 1px, transparent 60px),
@@ -51,7 +71,7 @@ const StyledCard = styled(Card)(props => {
         // maxHeight : '100px',
         // flex: '1 90%',
         // minWidth : '60px',
-        width : '80%',
+        width : '84%',
         height: '90%'
 })});
 
@@ -66,7 +86,7 @@ const StyledWrap = styled.div`
     background-color : ${YELLOW};
     box-shadow : 0 10px 20px rgba(0,0,0,0.19), 0 6px 6px rgba(0,0,0,0.23);
     border-radius: 4px;
-    width: 200px;
+    width: 300px;
     height: 80px;
     display: flex;
     flex-direction: column;
@@ -83,13 +103,27 @@ const StyledButton = styled(Button)`
     background-color : rgb(127, 177, 179);
     cursor : pointer;
     cursor : hand;
+    font-family : 'Work Sans', sans-serif;
+	font-weight : 300;
+    font-size : 18px;
+    color : rgb(77, 77, 77);
 
     :hover {
         background-color : rgb(174, 206, 208);
     }
 
+    :focus {
+        outline : none;
+    }
+
     :active {
         background-color: rgb(95, 158, 160);
+        outline: none;
+    }
+
+    > div {
+        text-align: center;
+        width: 100%;
     }
 `;
 
@@ -115,13 +149,13 @@ const HSVtoRGB = ( h, s, v ) => {
     return result;
 }
 
-function Button({className, callback}) {
+function Button({className, callback, title}) {
     return (
-        <a className={className} onClick={() => callback()}>
+        <button className={className} onClick={() => callback()}>
             <div>
-                Restart
+                {title}
             </div>
-        </a>
+        </button>
     )
 }
 
@@ -149,23 +183,143 @@ function AnimatedCard({isOpen, color, callback}) {
     )
 }
 
-function Alert({callback}) {
+function Alert({callback, alertTitle, round}) {
     return (
         <StyledAlertContainer>
             <StyledAlert>
                 <StyledWrap>
-                    <div>You won!</div>
-                    <StyledButton callback={callback}/>
+                    { alertTitle ? <div>{alertTitle}</div> : null }
+                    <StyledButton callback={callback} title={round ? 'Play Again' : 'Start'}/>
                 </StyledWrap>
             </StyledAlert>
         </StyledAlertContainer>
     )
 }
 
+const PENALTY = 5;
+const BONUS = 20;
+const MIN = 2;
+const SEC = 0;
+const TOTAL_TIME = MIN * 60 + SEC;
+const FRESH_TIME = `${MIN < 10 ? `0${MIN}` : MIN}:${SEC < 10 ? `0${SEC}` : SEC}`;
+
 const Flipcards = () => {
     const [cards, setCards] = useState([]);
     const [canFlip, setFlip] = useState(true);
-    const [completed, setCompleted] = useState(false);
+    const [running, setRunning] = useState(false);
+    const [alertTitle, setAlertTitle ] = useState('Match cards by color.');
+    const [round, setRound] = useState(0);
+    const [score, setScore] = useState(0);
+    const [isMatchKnown, setMatchKnown] = useState(false);
+
+    // stopwatch
+    const [timer, setTimer] = useState(TOTAL_TIME);
+    const [timeStr, setTimeStr] = useState(FRESH_TIME);
+
+    // only runs at initial render
+    useEffect(() => {
+        refresh();
+    },[]);
+
+    useEffect(() => {
+        let interval;
+
+        if (running) {
+            interval = setInterval(
+                () => {
+                    setTimer(timer - 1);
+                    calcShowTime(timer - 1);
+                },
+                1000,
+            );
+        }
+      
+        return () => clearInterval(interval);
+    }, [running, timer]);
+
+    useEffect(() => {        
+        const totalOpenCards = cards.filter(card => card.isOpen);  
+        const pendingCards = cards.filter(card => card.isOpen && card.isExposed);
+        if(totalOpenCards.length === cards.length && cards.length !== 0) {
+            setScore(score + BONUS + timer);
+            setRound(round + 1);
+            setAlertTitle(`Final score: ${score + BONUS + timer} (bonus ${timer})`);
+            setTimeout( () => setRunning(false), 500 );
+            return;
+        }
+
+        if(pendingCards.length === 2) {
+            setFlip(false);
+        } else if(pendingCards.length === 1) {
+            findKnownMatches(pendingCards[0]);
+        }
+
+        
+    }, [cards]);
+
+    useEffect(() => {
+        if(!canFlip) {
+            // close unmatched cards
+            const currentPair = cards.filter(card => card.isOpen && card.isExposed);
+            
+            if(currentPair.length === 2 && currentPair[0].color === currentPair[1].color) {
+                // if two cards are open and have matching color
+                // we can allow for the next pair to be opened
+                // and set their isExposed property to false
+                const updatedCards = cards.map(card => {
+                    if(card.isOpen && card.isExposed) {
+                        return { ...card, isExposed: false }
+                    }
+
+                    return card;
+                });
+                setScore(score + BONUS);
+
+                setCards(updatedCards);
+            } else {
+                // close unmatching cards
+                const updatedCards = cards.map(card => {
+                    if(card.isOpen && card.isExposed) {
+                        return { ...card, isOpen: false }
+                    }
+
+                    return card;
+                });
+
+                if(isMatchKnown) setScore(score - PENALTY);
+                setTimeout(() => setCards(updatedCards), 500);
+            }
+
+            setFlip(true);
+        }
+        
+    }, [canFlip, isMatchKnown]);
+
+    const findKnownMatches = (pendingCard) => {
+        // check if there was a card with the same color 
+        // that's been exposed but is currently closed
+        if(pendingCard) {
+            const matchingCard = cards.find(card => {
+                return !card.isOpen && card.isExposed && card.color === pendingCard.color;
+            });
+
+            setMatchKnown(matchingCard ? true : false);
+        }
+    };
+
+    const calcShowTime = (updatedTimer) => {
+        let minutes = Math.floor(updatedTimer / 60);
+        let seconds = Math.floor(updatedTimer % 60);
+        minutes = (minutes < 10) ? `0${minutes}` : minutes;
+        seconds = (seconds < 10) ? `0${seconds}` : seconds;
+        setTimeStr(`${minutes}:${seconds}`);
+    };
+
+    const startTimer = () => {
+        setTimer(TOTAL_TIME);
+        setTimeStr(FRESH_TIME);
+        setRunning(true); 
+    };
 
     const refresh = () => {
         let result = [];
@@ -175,57 +329,47 @@ const Flipcards = () => {
         for(var i = 0; i < 8; i++) {
             h += goldenRatio;
             h = h % 1;
-            result.push({color: HSVtoRGB(h, 0.5, 0.95), isOpen: false}, {color: HSVtoRGB(h, 0.5, 0.95), isOpen: false});
+            result.push({color: HSVtoRGB(h, 0.5, 0.95), isOpen: false, isExposed: false}, {color: HSVtoRGB(h, 0.5, 0.95), isOpen: false, isExposed: false});
         } 
         result.sort(() => 0.5 - Math.random());
 
         setCards(result);
-    }
-
-    useEffect(() => {
-        refresh();
-    },[]);
-
-    const closeUnmatchedCards = (updatedCards) => {
-        const result = updatedCards.map((card, i) => {
-            if(card.isOpen) {
-                const pair = updatedCards.filter(c => card.color === c.color && c.isOpen);
-                if( pair.length % 2 !== 0 ) {
-                    return {...card, isOpen: !card.isOpen}
-                }
-            }
-    
-            return card;
-        });
-        setFlip(true);
-        setCards(result);
+        setScore(0);
     };
 
     const updateCards = (index) => {
-        if(canFlip) {
+        // don't allow closing card if it's been opened
+        if(canFlip && cards[index] && !cards[index].isOpen) {
             const updatedCards = cards.map( (card, i) => {
-                return i === index ? { ...card, isOpen: !card.isOpen } : card;
-            } )
-            const openCards = updatedCards.filter(card => card.isOpen);
+                return i === index ? { ...card, isOpen: !card.isOpen, isExposed: true } : card;
+            } );
+            
             setCards(updatedCards);
-    
-            if(openCards.length % 2 === 0 && openCards.length < cards.length) {
-                setFlip(false);
-                setTimeout(() => closeUnmatchedCards(updatedCards), 700);
-            } else if(openCards.length === cards.length) {
-                setTimeout( () => setCompleted(true), 800 );
-            }
         }
-    };
+    }
 
     const restart = () => {
-        setCompleted(false);
-        refresh();
-    }
+        setCards(cards.map(card => ({...card, isOpen: false})));
+        setTimeout(() => {
+            refresh();
+            startTimer();
+        }, 500);
+    };
 
     return (
         <div>
-            <StyledCardsContainer completed={completed}>
+            <StyledHeaderContainer>
+                <div>
+                    <div>{timeStr}</div>
+                </div>
+                <div>
+                    <div>Score: {score}</div>
+                </div>
+                <div>    
+                    <StyledButton callback={restart} title={'Restart'}/>
+                </div>
+            </StyledHeaderContainer>
+            <StyledCardsContainer running={running}>
                 {cards.map((card, index) => {
                     return (
                         <AnimatedCard
@@ -237,7 +381,7 @@ const Flipcards = () => {
                     )
                 })}
             </StyledCardsContainer>
-            {completed ? <Alert callback={restart}/> : null}
+            {!running ? <Alert callback={restart} alertTitle={alertTitle} round={round}/> : null}
         </div>
     )
 }
